ab253a8695af8200025b02a6aef66944df90dd29
Kammal <kamalrsa@gmail.com>
2021-04-15 15:25:32 +0200

    added projects

---
+++ project.js
@@ -0,0 +1,17 @@
+const button = document.getElementById("meal1");
+button.addEventListener("click", (e) => {
+  location.assign("https://github.com/kamalrsa/meal_sharingHYF");
+});
+document.getElementById("meal2").onclick = function () {
+  location.href = "https://restaurantcph.herokuapp.com";
+};
+document.getElementById("cv1").onclick = function () {
+  location.href = "https://github.com/HackYourFuture-CPH/rate-my-cv";
+};
+document.getElementById("Weather").onclick = function () {
+  location.href =
+    "https://github.com/kamalrsa/hyf-homework/blob/master/javascript/javascript3/week1/weather.js";
+};
+document.getElementById("fizzit").onclick = function () {
+  location.href = "https://fizzit.green/";
+};
