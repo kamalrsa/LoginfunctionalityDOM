3e0e27b166eddd5c376d6a4104826113dd40e9cc
Kammal <kamalrsa@gmail.com>
2021-04-16 11:27:43 +0200

    changes in login functionality

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-const user = [
+/* const user = [
   { username: "kamal", password: "Kk12345678" },
   { username: "kamal12", password: "Kk12345678" },
 ];
@@ -15,6 +15,30 @@ function getInfo() {
       alert("doesnot match");
     }
   }
-}
+} */
 
-//const button = document.getElementById("");
+function getInfo() {
+  const username = document.getElementById("uname").value;
+  const password = document.getElementById("psw").value;
+  let username_pattern = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  let password_pattern = /(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
+
+  if (username == "") {
+    alert("please enter user name.");
+  } else if (password == "") {
+    alert("enter the password");
+  } else if (!username_pattern.test(username)) {
+    alert("Enter valid email id.");
+  } else if (!password_pattern.test(password)) {
+    alert(
+      "Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
+    );
+  } else {
+    window.location = "https://www.campuslife.co.in";
+  }
+}
+//Reset Inputfield code.
+function clearFunc() {
+  const username = (document.getElementById("uname").value = "");
+  const password = (document.getElementById("psw").value = "");
+}
