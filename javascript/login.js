8c20ca1262992f4b16b688e0bf8b734e36b1c8b5
Kammal <kamalrsa@gmail.com>
2021-04-18 23:25:48 +0200

    registeration and login

--- javascript/login.js
+++ javascript/login.js
@@ -1,23 +1,49 @@
-/* const user = [
-  { username: "kamal", password: "Kk12345678" },
-  { username: "kamal12", password: "Kk12345678" },
-];
+const user = [];
+function newUser() {
+  const signupUser = document.getElementById("newuname").value;
+  const signupEmail = document.getElementById("email").value;
+  const signupPassword = document.getElementById("newpsw").value;
+  const newUser = {
+    username: signupUser,
+    email: signupEmail,
+    password: signupPassword,
+  };
+  let email_pattern = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  let password_pattern = /(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
 
+  if (signupUser == "") {
+    alert("please enter user name.");
+  } else if (signupEmail == "") {
+    alert("enter the password");
+  } else if (signupPassword == "") {
+    alert("enter the password");
+  } else if (!email_pattern.test(signupEmail)) {
+    alert("Enter valid email id.");
+  } else if (!password_pattern.test(signupPassword)) {
+    alert(
+      "Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
+    );
+  } else {
+    user.push(newUser);
+    console.log(user);
+    alert("Registration Complete!!!");
+  }
+}
 function getInfo() {
   const username = document.getElementById("uname").value;
   const password = document.getElementById("psw").value;
 
   for (let i = 0; i < user.length; i++) {
-    if (username == user[i].username && password == user[i].password) {
-      location.assign("http://w3schools-fa.ir/jsref/met_loc_assign.html");
+    if (username == user[i].email && password == user[i].password) {
+      window.location = "https://www.campuslife.co.in";
       return;
     } else {
       alert("doesnot match");
     }
   }
-} */
+}
 
-function getInfo() {
+/* function getInfo() {
   const username = document.getElementById("uname").value;
   const password = document.getElementById("psw").value;
   let username_pattern = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
@@ -36,9 +62,14 @@ function getInfo() {
   } else {
     window.location = "https://www.campuslife.co.in";
   }
-}
+} */
 //Reset Inputfield code.
-function clearFunc() {
+function clearLoginFunc() {
   const username = (document.getElementById("uname").value = "");
   const password = (document.getElementById("psw").value = "");
+}
+function clearSignupFunc() {
+  const signupUser = (document.getElementById("newuname").value = "");
+  const signupEmail = (document.getElementById("email").value = "");
+  const signupPassword = (document.getElementById("newpsw").value = "");
 }
